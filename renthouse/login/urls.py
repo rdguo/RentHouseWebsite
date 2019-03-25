b562cb580005a596b41b20cbbb968a422f95cb32
huifeng-kooboo <gisdoing@gmail.com>
2019-03-25 23:46:42 +0800

    用户注册和登录界面 和简单功能实现

--- renthouse/login/urls.py
+++ renthouse/login/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login',views.loginSystem, name = 'loginSystem'),
-    path('indexman',views.indexman,name = 'indexman')
+    path('indexman',views.indexman,name = 'indexman'),
+    path('register',views.registerSystem,name = 'registerSystem') #注册用户信息
 ]
--- renthouse/login/views.py
+++ renthouse/login/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from .models import LoginInfo
-# Create your views here.
 from django.http import HttpResponse
 
 #测试用例
@@ -8,17 +7,23 @@ def index(request):
     return HttpResponse("Hello, world. ") #test
 
 def indexman(request):
-    return HttpResponse("Hello, world. ss") #test
+    return render(request,"login/register.html")
 
+'''登录系统功能'''
 def loginSystem(request):
-    modellist = LoginInfo.objects.all()
-    if request.method == "POST":
-        user_name = request.POST.get("login_name",None) #获得用户名
-        pass_word = request.POST.get("login_pwd",None)  #获得密码
-        save_obj = LoginInfo(username = user_name,password= pass_word) #添加到数据库当中
-        save_obj.save()
-        if user_name == "123" and pass_word == "123":
-            return redirect('index',{'modellist':modellist})
+    userinfoList = LoginInfo.objects.all() #得到用户信息数据库列表  就是包含所有用户信息
+    if request.method == "POST": #当传递参数后 submit
+        user_name = request.POST.get("username",None) #获得输入的用户名
+        pass_word = request.POST.get("password",None)  #获得输入的密码
+        search_obj = LoginInfo.objects.get(username=user_name,password=pass_word) #判断数据库当中是否存在
+        search_count = len(search_obj)
+        if search_count == 1: #说明找到
+            return redirect('indexman') #重定向到登录成功页面
         else:
-            return redirect("indexman")
-    return render(request,'login/login.html',{'modellist':modellist})
+            print("当前密码或用户名输错")
+            return render(request, 'login/login.html', {'modellist': userinfoList})
+    return render(request,'login/login.html',{'modellist':userinfoList})
+
+'''注册界面功能'''
+def registerSystem(request):
+    return render(request,'login/regist.html')
